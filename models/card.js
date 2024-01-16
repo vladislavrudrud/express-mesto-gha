a8d778b88af4af4b129c631bcd66a2058ee2ee12
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-17 02:05:35 +0300

    24

--- models/card.js
+++ models/card.js
@@ -5,9 +5,9 @@ const cardSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Поле должно быть заполнено'],
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
+      required: [true, 'Поле должно быть заполнено'],
     },
     link: {
       type: String,
@@ -22,11 +22,13 @@ const cardSchema = new mongoose.Schema(
       ref: 'user',
       default: [],
     },
-    likes: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      default: [],
-    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        default: [],
+      },
+    ],
     createdAt: {
       type: Date,
       default: Date.now,
