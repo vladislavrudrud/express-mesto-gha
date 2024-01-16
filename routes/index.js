f7e4adb5e7b62561b7a9d7c14ade2bf133dca57f
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-17 01:00:55 +0300

    70

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const { Router } = require('express');
 const { userRouter } = require('./users');
 const { cardRouter } = require('./cards');
-const { NotFound } = require('../utils/errors');
+// const { NotFound } = require('../utils/errors');
 
 const router = Router();
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-router.all('/*', (req, res) => {
-  res
-    .status(NotFound.status)
-    .send({ message: NotFound.message });
-});
+// router.all('/*', (req, res) => {
+//   res
+//     .status(NotFound.status)
+//     .send({ message: NotFound.message });
+// });
 
 module.exports = { router };
