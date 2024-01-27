685c19fec9dc8f5ae1bd218dd41c865ec3a9bdda
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-27 15:50:23 +0300

    14pw13

--- models/user.js
+++ models/user.js
@@ -7,18 +7,21 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      required: false,
       default: 'Жак-Ив Кусто',
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
     },
     about: {
       type: String,
+      required: false,
       default: 'Исследователь',
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
     },
     avatar: {
       type: String,
+      required: false,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: {
         validator(url) {
