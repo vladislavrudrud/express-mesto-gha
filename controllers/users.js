a24b638f95b3fadf875c0f2ab091ea6553ed4eac
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-29 15:51:52 +0300

    14pw16

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,30 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const CREATED = require('../utils/constants');
 const ConflictError = require('../utils/conflicterror');
 const BadRequestError = require('../utils/badrequesterror');
 const NotFoundError = require('../utils/notfounderror');
 
+const getUser = (req, res, next) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(next);
+};
+const getUserById = (req, res, next) => {
+  User.findById(req.params.idUser)
+    .orFail(new Error('NotFound'))
+    .then((user) => res.send(user))
+    .catch((error) => {
+      if (error.name === 'CastError') {
+        next(new BadRequestError('Ошибка! Идентификатор недопустим!'));
+      } else if (error.message === 'NotFound') {
+        next(new NotFoundError('Пользователь не найден!'));
+      } else {
+        next(error);
+      }
+    });
+};
 const createUser = (req, res, next) => {
   const {
     name,
@@ -26,25 +45,6 @@ const createUser = (req, res, next) => {
         next(new ConflictError('Ошибка! Данные уже используются!'));
       } else if (error.name === 'ValidationError') {
         next(new BadRequestError(error.message));
-      } else {
-        next(error);
-      }
-    });
-};
-const getUser = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch(next);
-};
-const getUserById = (req, res, next) => {
-  User.findById(req.params.idUser)
-    .orFail(new Error('NotFound'))
-    .then((user) => res.send(user))
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        next(new BadRequestError('Ошибка! Идентификатор недопустим!'));
-      } else if (error.message === 'NotFound') {
-        next(new NotFoundError('Пользователь не найден!'));
       } else {
         next(error);
       }
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const UnauthorizedError = require('../utils/unauthorized');
@@ -7,21 +7,18 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: false,
       default: 'Жак-Ив Кусто',
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
     },
     about: {
       type: String,
-      required: false,
       default: 'Исследователь',
       minlength: [2, 'Минимальная длина поля 2 символа'],
       maxlength: [30, 'Максимальная длина поля 30 символов'],
     },
     avatar: {
       type: String,
-      required: false,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: {
         validator(url) {
