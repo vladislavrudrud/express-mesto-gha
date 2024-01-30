b5465ca5c07fb40f9e00f14fe880f0ba6db8193d
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-30 15:59:07 +0300

    14pw43

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 const signInRouter = require('./signin');
 const signUpRouter = require('./signup');
-const authMiddleware = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 router.use('/signup', signUpRouter);
 router.use('/signin', signInRouter);
-router.use(authMiddleware);
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
