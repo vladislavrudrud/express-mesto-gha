a18d2dfb0b02079426fa1697e8ab5a33690f37bc
Владислав Рудь <kolosovvvskiy@yandex.ru>
2024-01-30 15:34:29 +0300

    14pw40

--- routes/users.js
+++ routes/users.js
@@ -10,23 +10,23 @@ const {
 } = require('../controllers/users');
 const { REGEX } = require('../utils/constants');
 
-userRouter.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().regex(REGEX),
-  }),
-}), editUserAvatar);
 userRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), editUserInfo);
-userRouter.get('/me', getUserInfo);
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(REGEX),
+  }),
+}), editUserAvatar);
 userRouter.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserById);
+userRouter.get('/me', getUserInfo);
 userRouter.get('/', getUser);
 
 module.exports = userRouter;
